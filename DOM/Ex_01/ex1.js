2fb6a520e1c664b34ede0f8e4efa9c212173eea7
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-10 23:37:31 +0000

    Javascript Questions and Answers

--- DOM/Ex_01/ex1.js
+++ DOM/Ex_01/ex1.js
@@ -1,10 +1,12 @@
 // Create a <button>Hello</button>. Use document.querySelcetor('button') to get the button from 
 // the page, and then display it in the console. 
 
-// Solution 
+//  1st Solution 
 
 const buttonElement = document.querySelector('button')
 console.log(buttonElement);
 
+
+// 2nd Solution
 
 console.log(document.querySelector('button'));
