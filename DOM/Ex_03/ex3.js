e6e81457a01b05762a2df1396544c80cb3dc37e8
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-11 00:49:59 +0000

    Javascript Questions and Answers

---
+++ DOM/Ex_03/ex3.js
@@ -0,0 +1,6 @@
+// Create 2 buttons 'heads' and 'tails', and a paragraph (<p></p>) underneath. 
+// When clicking the 'heads' button, display 'You chose"heads' in the paragraph. 
+// When the 'tails' button, display 'You chose: tails'.
+
+
+// Solution 
