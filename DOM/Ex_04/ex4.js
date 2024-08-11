0a60ab4b5a2ed9ae3457a0268869019fc5686986
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-11 22:02:50 +0000

    Javascript Questions and Answers

---
+++ DOM/Ex_04/ex4.js
@@ -0,0 +1,3 @@
+// Create a text box (<input>) and submit button. When clicking 'submit', display 
+//'Your name is: ${text};' on the page (${text} = the text inside the box) (hint: use.value
+// to get the text inside an <input>).
