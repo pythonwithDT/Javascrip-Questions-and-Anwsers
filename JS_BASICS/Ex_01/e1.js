c8c3cd2f8106d5e83958b11ac2c1308c6c3d1886
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-12 19:37:29 +0000

    Javascript Questions and Answers

--- JS_BASICS/Ex_01/e1.js
+++ JS_BASICS/Ex_01/e1.js
@@ -1,1 +1,20 @@
 // Use alert(...); to display 'Good morining!' in a popup. 
+
+alert('Good morning!');
+
+// Display your name in a popup 
+
+alert('Emmanuel Tetteh!')
+
+// Use math, calucluate 10 + 5 in the console 
+
+console.log(10 + 5);
+
+// Calculate 20 -5 in the console 
+
+console.log(20 - 5); 
+
+
+// calculate 2 + 2 - 5 in the console. 
+
+console.log(2 + 2 - 5);
