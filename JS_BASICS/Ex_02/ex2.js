23fe1ab521a8bdb98fb5b7928f90455b6c1023b9
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-12 19:42:54 +0000

    Javascript Questions and Answers

---
+++ JS_BASICS/Ex_02/ex2.js
@@ -0,0 +1,13 @@
+// Use document.body.innerHTML = ...; to display 'Good morning!' on the webpage 
+
+// Solution 
+
+document.body.innerHTML= 'Good morning'
+
+
+
+// Display your name on the webpage. 
+
+// Solution
+
+document.body.innerHTML = 'Emmanuel Dortey Tetteh'
