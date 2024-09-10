43672f55e0b6d7ca82d8f7fd760de8446a5e2a37
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-10 03:26:48 +0000

    amazon project

---
+++ CHALLENGE EXERCISE/HTML AND CSSS REVIEW/main.js
@@ -0,0 +1,4 @@
+// Add onclick=".." attributes to the two buttons:
+// - when clicking "Add to cart", create a popup with the text "Added"
+// - when clicking "Buy now", display the message "loading..."" in the console. 
+// and then create a popup with the text "Purchased"
