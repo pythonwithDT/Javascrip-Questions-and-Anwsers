d68dd6be3233544c12f9b7fdf3090310874cf557
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-10 00:19:00 +0000

    amazon project

--- CHALLENGE EXERCISE/JS_BASICS/Ex_01/main.js
+++ CHALLENGE EXERCISE/JS_BASICS/Ex_01/main.js
@@ -1,4 +1,10 @@
-// You order a T-shirt for $10, sockd for $8, and dinner plates for 20$
+// You order a T-shirt for $10, socks for $8, and dinner plates for 20$
 //Use Javascript to calculat the total cost of your order.
 
 // Solution
+
+let T_shirt = 10;
+let socks = 8;
+let plates = 20;
+let total = T_shirt + socks + plates;
+console.log(total);
