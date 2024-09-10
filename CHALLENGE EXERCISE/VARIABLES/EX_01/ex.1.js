9268f9e700eaa425fb39fe5fe00e9b1b169b7444
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-10 22:27:58 +0000

    created the variable with the empty string

---
+++ CHALLENGE EXERCISE/VARIABLES/EX_01/ex.1.js
@@ -0,0 +1,18 @@
+// Creata varable called "calculation", and save an empty string inside: 
+//let calculation = "";
+// (this variable will store the calucation like '1 +2' or '11 + 2 +2)
+
+// Solution
+
+let calculation = "";
+
+
+
+// when we click the '1' button:
+// - add the string '1' to the "calculation" variable: calucation += '1';
+// - display the calculation in the console: console.log(calculation);
+
+document.querySelector('.js-button1').addEventListener('click', () => {
+    calculation += '1';
+    console.log(calculation);
+})
