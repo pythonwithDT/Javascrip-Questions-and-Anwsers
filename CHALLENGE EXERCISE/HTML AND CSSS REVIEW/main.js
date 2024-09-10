17769f836f1978db9883b89be24ffd49f3f7457e
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-10 03:59:18 +0000

    amazon project

--- CHALLENGE EXERCISE/HTML AND CSSS REVIEW/main.js
+++ CHALLENGE EXERCISE/HTML AND CSSS REVIEW/main.js
@@ -2,3 +2,15 @@
 // - when clicking "Add to cart", create a popup with the text "Added"
 // - when clicking "Buy now", display the message "loading..."" in the console. 
 // and then create a popup with the text "Purchased"
+
+
+// Solution
+
+document.querySelector('.js-button1').addEventListener('click', () => {
+    alert('Added');
+});
+
+document.querySelector('.js-button2').addEventListener('click', () => {
+    console.log('loading...')
+    alert('Purchased');
+});
