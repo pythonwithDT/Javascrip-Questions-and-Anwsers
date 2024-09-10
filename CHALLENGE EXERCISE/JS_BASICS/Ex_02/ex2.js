039c0f5d5096f7573ec6a0e79768e2c3641fb70a
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-10 00:29:18 +0000

    amazon project

---
+++ CHALLENGE EXERCISE/JS_BASICS/Ex_02/ex2.js
@@ -0,0 +1,11 @@
+// Your bank account has $100, you spend $20 on lunch, 50$ on dinner, earn %200 from your job  
+// Claculate how much mooney you have. 
+
+// Solution
+
+let bankAccount = 100;
+let lunch = 20;
+let dinner = 50;
+let job = 200;
+let total = bankAccount - lunch - dinner + job;
+console.log(total);
