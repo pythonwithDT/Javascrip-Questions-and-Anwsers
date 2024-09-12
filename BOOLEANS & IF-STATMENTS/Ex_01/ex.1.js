a9988a4f8934376bafeb26b685400992c5a76b32
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-12 23:52:36 +0000

    added html and css file for booleans and if statements for ex01

---
+++ BOOLEANS & IF-STATMENTS/Ex_01/ex.1.js
@@ -0,0 +1,8 @@
+// Create a variable called "hour" and save the current houe of the day
+// (use 24-hour format and save it as a number between 0 and 23). 
+// - if hour is between 6 and 12, display "Good morning!" inthe console
+// - if hour is between 13 and 17, display "Goos afternoon!" in the console 
+// - otherwise, display "Good night!" in the console
+
+// Solutions
+
