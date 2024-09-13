5da97fa0adc50e11686f11708cc37a908e31ac2b
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-13 04:29:53 +0000

    worked on fist question in ex2

---
+++ BOOLEANS & IF-STATMENTS/Ex_02/ex.2.js
@@ -0,0 +1,16 @@
+// Generate a random number with Math.random(). Save it in a variable 
+// create an if-statement and check:
+// -if the number is less than 0.5, then display 'heads' in the console.
+// -else display 'tails' in the console. 
+
+
+// Solution
+
+const Number = Math.random()
+
+if ( number => 0.5) {
+    console.log('heads')
+} else {
+    console.log('tail')
+}
+
