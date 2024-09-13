5da47ba4be4be238ca5a4d792b7c37252e9032c9
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-13 03:52:23 +0000

    gave the solution for the first question

--- BOOLEANS & IF-STATMENTS/Ex_01/ex.1.js
+++ BOOLEANS & IF-STATMENTS/Ex_01/ex.1.js
@@ -1,8 +1,17 @@
 // Create a variable called "hour" and save the current houe of the day
 // (use 24-hour format and save it as a number between 0 and 23). 
-// - if hour is between 6 and 12, display "Good morning!" inthe console
+// - if hour is between 6 and 12, display "Good morning!" int the console
 // - if hour is between 13 and 17, display "Goos afternoon!" in the console 
 // - otherwise, display "Good night!" in the console
 
 // Solutions
 
+const hour = 3
+
+if (hour <= 6 && hour <= 12) {
+    console.log('Good morning')
+} else if (hour <= 13 && 17 ) {
+    console.log('Good afternoon')
+} else {
+    console.log('Good night')
+}
