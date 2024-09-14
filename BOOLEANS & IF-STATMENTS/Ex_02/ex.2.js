86aa7ebf81aee29f0d5d6f7e149e331fdd2839d1
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-14 21:00:55 +0000

    added a question

--- BOOLEANS & IF-STATMENTS/Ex_02/ex.2.js
+++ BOOLEANS & IF-STATMENTS/Ex_02/ex.2.js
@@ -27,3 +27,7 @@ if ( Number < 0.5) {
     result = 'tails'
 }
 console.log(result)
+
+// Lets say we're trying to guess the result. Create a variable called 'guess' and vae your guess ('heads' or 'tails').
+// - if you guess matches the result, display 'You win!' in the console
+// if your guess does not match the result, display 'You lose!'
